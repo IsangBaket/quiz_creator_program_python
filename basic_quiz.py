7a51c75d728dec2253a4258b78bed9a4d6d113fb
IsangBaket? <max.bias@myyahoo.com>
2025-04-09 17:46:23 +0800

    test lang

---
+++ basic_quiz.py
@@ -0,0 +1,11 @@
+# For Assignment 9: Quiz Creator
+# Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
+# Write the collected data to a text file. Ask another question until the user chose to exit.
+
+print("This simple quiz tests how much you know Max Austine!")
+
+ques1 = input("What is Max's fav food?: ")
+print("A. Adobo " \
+    "B. Nilagang Baka" \
+    "C. Fried Sunny Side Egg" \
+    "D. McDonald's Chicken Fillet")
