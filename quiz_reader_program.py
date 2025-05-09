c14ef262bcfbb7883bfb9f954d3d3d4067fcbddd
IsangBaket? <max.bias@myyahoo.com>
2025-05-09 15:05:17 +0800

    Added function for getting text file from user. Also added message for FileNotFound error

--- quiz_reader_program.py
+++ quiz_reader_program.py
@@ -2,7 +2,16 @@
 # The user will answer the randomly selected question and check if the answer is correct.
 
 def file_name():    # added needed functions first
-    pass
+    while True:
+        try:     # asks for user file name to read
+            txt_file = input("file name with '.txt': ")
+            if txt_file.endswith('.txt'):   # ensures that user puts file extension
+                break
+            with open(txt_file):
+                pass
+        except FileNotFoundError:
+            print("File not found")
+    return txt_file
 
 def convert_quiz():
     pass
@@ -12,4 +21,5 @@ def quiz():
 
 def main():
     pass
+
 
