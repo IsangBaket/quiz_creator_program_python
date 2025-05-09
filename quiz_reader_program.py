22b6d73754e30f32c2121feb9f91a22069c77d0c
IsangBaket? <max.bias@myyahoo.com>
2025-05-09 16:49:22 +0800

    Added function body for convert_quiz Note: this was the hardest part but I got it somehow I was just have to split the data in blocks using the .split() method

--- quiz_reader_program.py
+++ quiz_reader_program.py
@@ -13,8 +13,26 @@ def file_name():    # added needed functions first
             print("File not found")
     return txt_file
 
-def convert_quiz():
-    pass
+def convert_quiz(txt_file):
+    with open(txt_file, "r") as file:
+        content = file.read()
+
+    raw_questions = content.strip().split('------\n\n')
+    quiz_data = []
+
+    for item in raw_questions:
+        lines = item.strip().split('\n')
+        if len(lines) >= 6:
+            question_data = {
+                'question': lines[0][9:],  # remove "Question: "
+                'A': lines[1][3:],         # remove "A: "
+                'B': lines[2][3:],
+                'C': lines[3][3:],
+                'D': lines[4][3:],
+                'answer': lines[5][-1].upper()  # get last character (letter of correct answer)
+            }
+            quiz_data.append(question_data)
+    return quiz_data
 
 def quiz():
     pass
